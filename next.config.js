75e71a2bd1c2b3c4be0127759e3aa88d76f4d44e
nipunadulara <flangodev@gmail.com>
2024-10-11 06:23:07 +0530

    issue resolved -> next.config.js changed

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
+
+const path = require('path');
+
 module.exports = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    return config;
+  },
   poweredByHeader: false
 };
